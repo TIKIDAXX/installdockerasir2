cce4dfeb1897013cba381e2c1a42854001535d89
TIKIDAXX <HAP.MESTRE77@GMAIL.COM>
2025-10-20 18:51:27 +0200

    Refactor Docker installation in instdocker.sh
    Updated Docker installation script to use Docker's official repository and added necessary dependencies.

--- instdocker.sh
+++ instdocker.sh
@@ -1,2 +1,5 @@
-sudo apt install -y docker-ce docker-ce-cli containerd.io \
-docker-buildx-plugin docker-compose-plugin
+apt install apt-transport-https ca-certificates curl software-properties-common
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt install docker.io
